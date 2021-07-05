a900cfc5337500d6d2a475f0acf58e3d7135dd19
Wotts <twan.3ssen@gmail.com>
2021-07-05 21:00:03 +0200

    User construct roles as array

--- app/src/Entity/User.php
+++ app/src/Entity/User.php
@@ -3,7 +3,6 @@
 namespace App\Entity;
 
 use App\Repository\UserRepository;
-use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
@@ -56,7 +55,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
     public function __construct()
     {
-        $this->roles = new ArrayCollection();
+        $this->roles = [];
         $this->plainPassword = 'henk';
     }
 
